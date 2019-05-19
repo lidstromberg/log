85295bff5f061324688aed72253d1e30715663c0
lidstromberg <mcnandezzz@googlemail.com>
2019-05-19 17:55:57 +0200

    updated log source

--- logger.go
+++ logger.go
@@ -10,14 +10,13 @@ import (
 //LogEvent writes a bunch of messages to the output source
 func LogEvent(messages ...string) {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.LUTC)
-	log.SetOutput(os.Stderr)
+	log.SetOutput(os.Stdout)
 	log.Println(strings.Join(messages, ":"))
 }
 
 //IsLoggingOn uses a supplied string to determine if logging is on
 func IsLoggingOn(key string) bool {
 	result, err := strconv.ParseBool(key)
-
 	if err != nil {
 		log.Printf("IsLoggingOn could not parse %s", key)
 		return false
